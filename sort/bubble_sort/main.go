cffc52d3b093a6669cb61e2fb69d2a3c937c37dd
Kenjiro Kuge <kugetyan0211@gmail.com>
2026-01-04 14:34:44 +0900

    feat: Bubbleソート

---
+++ sort/bubble_sort/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("Bubble Sort")
+	arr := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		arr[i] = rand.Intn(10)
+	}
+	start := time.Now()
+	fmt.Println(arr)
+	arr = bubbleSort(arr)
+	end := time.Since(start)
+	fmt.Println(arr)
+	fmt.Println(end)
+}
+
+func bubbleSort(arr []int) []int {
+	for i := 0; i < len(arr) - 1; i++ {
+		for j := 0; j < len(arr) - 1 - i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
