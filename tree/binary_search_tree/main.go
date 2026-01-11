bbdd71ff91ee28ad61df3902e2f83866eac952ca
Kenjiro Kuge <kugetyan0211@gmail.com>
2026-01-11 23:23:46 +0900

    feat: Binary Search TreeのInOrderとSearch

--- tree/binary_search_tree/main.go
+++ tree/binary_search_tree/main.go
@@ -15,6 +15,19 @@ func main() {
 	root = root.Insert(root, 2)
 	fmt.Println(root.left.value)
 	fmt.Println(root.left.right.value)
+
+	fmt.Println("--------------------------------")
+	root.InOrder()
+	fmt.Println("--------------------------------")
+	root.PreOrder()
+	fmt.Println("--------------------------------")
+	root.PostOrder()
+	fmt.Println("--------------------------------")
+
+	fmt.Printf("Search 10: %v\n", root.Search(10))
+	fmt.Printf("Search 5: %v\n", root.Search(5))
+	fmt.Printf("Search 9: %v\n", root.Search(9))
+	fmt.Printf("Search 4: %v\n", root.Search(4))
 }
 
 type Node struct {
@@ -35,5 +48,42 @@ func (n *Node) Insert(node *Node, value int) *Node{
 	}
 
 	return node
+}
+
+func (n *Node) InOrder() {
+	if n != nil {
+		n.left.InOrder()
+		fmt.Println(n.value)
+		n.right.InOrder()
+	}
+}
+
+func (n *Node) PreOrder() {
+	if n != nil {
+		fmt.Println(n.value)
+		n.left.PreOrder()
+		n.right.PreOrder()
+	}
+}
+
+func (n *Node) PostOrder() {
+	if n != nil {
+		n.left.PostOrder()
+		n.right.PostOrder()
+		fmt.Println(n.value)
+	}
+}
+
+func (n *Node) Search(value int) bool {
+	if n == nil {
+		return false
+	}
+	if value == n.value {
+		return true
+	} else if value < n.value {
+		return n.left.Search(value)
+	} else {
+		return n.right.Search(value)
+	}
 }
 
