9e6c956f3e01c50c4d4156c349a6bfe05761598c
Kenjiro Kuge <kugetyan0211@gmail.com>
2026-01-11 18:33:26 +0900

    feat: クイズ - JSONのカッコのフォーマットを正せ

--- stack_queue/stack/main.go
+++ stack_queue/stack/main.go
@@ -15,6 +15,11 @@ func main() {
 	fmt.Println(s.stack)
 	fmt.Println(s.pop())
 	fmt.Println(s.stack)
+
+	j := "{'key1': 'value1', 'key2': [1, 2, 3], 'key3': {'key4': 'value4'}}"
+	fmt.Printf("Is valid format: %t\n", validateFormat(j))
+	j = "{'key1': 'value1', 'key2': [1, 2, 3], 'key3': {'key4': 'value4'}}]"
+	fmt.Printf("Is valid format: %t\n", validateFormat(j))
 }
 
 type Stack struct {
@@ -33,4 +38,36 @@ func (s *Stack) pop() any {
 	}
 
 	return nil
+}
+
+func validateFormat(chars string) bool {
+	lookup := map[string]string{
+		"{": "}",
+		"[": "]",
+		"(": ")",
+	}
+	lookupValues := map[string]bool{}
+	for _, value := range lookup {
+		lookupValues[value] = true
+	}
+
+	s := Stack{}
+	for _, char := range chars {
+		if _, ok := lookup[string(char)]; ok {
+			s.push(lookup[string(char)])
+			continue
+		}
+		
+		if _, ok := lookupValues[string(char)]; ok {
+			if len(s.stack) == 0 {
+				return false
+			}
+			if s.pop() != string(char) {
+				return false
+			}
+			continue
+		}
+	}
+
+	return len(s.stack) == 0
 }
